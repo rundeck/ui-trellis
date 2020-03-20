69631c2238b137552eb75a1200b212ce3332402e
Greg Schueler <greg.schueler@gmail.com>
2020-03-20 09:26:03 -0700

    Use vue property decorator

--- src/components/utils/PageConfirm.vue
+++ src/components/utils/PageConfirm.vue
@@ -1,52 +1,50 @@
 <template>
-  <span ><slot v-if="display && needsConfirm" :confirm="confirm" :needs-confirm="needsConfirm">{{message}}</slot></span>
+  <span ><slot v-if="display && needsConfirm" :confirm="confirmData" :needs-confirm="needsConfirm">{{message}}</slot></span>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({
-  props:{
-    eventBus:{
-      type:Vue,required:true
-    },
-    message:{
-      type:String, required:true
-    },
-    display:{
-      type:Boolean, default: false
+import {Component, Prop} from 'vue-property-decorator'
+export default class PageConfirm extends Vue{
+  confirmData:  string[] = []
+
+  @Prop({required:true})
+  eventBus!:Vue
+
+  @Prop({required:true})
+  message!:String
+
+  @Prop({required:true})
+  display!:Boolean
+
+
+
+  setConfirm(name:string){
+    const loc=this.confirmData.indexOf(name)
+    if(loc<0){
+      this.confirmData.push(name)
     }
-  },
-  methods:{
-    setConfirm(name:string){
-      const loc=this.confirm.indexOf(name)
-      if(loc<0){
-        this.confirm.push(name)
-      }
-    },
-    resetConfirm(name:string){
-      const loc=this.confirm.indexOf(name)
-      if(loc>=0){
-        this.confirm.splice(loc,1)
-      }
+  }
+  resetConfirm(name:string){
+    const loc=this.confirmData.indexOf(name)
+    if(loc>=0){
+      this.confirmData.splice(loc,1)
     }
-  },
-  data(){
-    return{
-      confirm: [] as string[]
-    }
-  },
-  computed: {
-    needsConfirm: function():boolean {
-      return this.confirm.length>0
-    }
-  },
+  }
+
+
+  get needsConfirm():boolean {
+    return this.confirmData.length>0
+  }
+
   mounted(){
     this.eventBus.$on('page-modified',this.setConfirm)
     this.eventBus.$on('page-reset',this.resetConfirm)
     window.onbeforeunload = ()=> {
-        if (this.needsConfirm) {
-            return this.message||'confirm'
-        }
+      if (this.needsConfirm) {
+        return this.message||'confirm'
+      }
     }
+
   }
-})
+}
 </script>
